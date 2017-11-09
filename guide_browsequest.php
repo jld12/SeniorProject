b17f1266218864ba30124ccfe07b7b5f4cfefc19
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:04:03 -0500

    guide_browsequest

---
+++ guide_browsequest.php
@@ -0,0 +1,35 @@
+<?php
+
+// A simple guide for browsing quests.
+
+$page_title = 'Quest Browsing Guide';
+include ('includes/header.php');
+?>
+
+<h1>Browsing PSO2 Quests</h1>
+<h4></h4>
+
+<fieldset>
+<h3>Step 1: Click on the 'Search' button at the top or head to <a href="https://phantasy.online/search.php">phantasy.online/search.php</a>.</h3>
+<img src="guideimages/guide_browse_1.png" border="4"/>
+<p></p><br />
+<hr>
+<h3>Step 2: Search using one of two available methods:</h3>
+<img src="guideimages/guide_browsequest_2.png" border="4"/>
+<p>You can search for a quest by name (3a) or zone (3b). To see a specific method, head to one of those steps below.</p><br />
+<hr>
+<h3>Step 3a: By name method:</h3>
+<img src="guideimages/guide_browsequest_3.png" border="4"/>
+<p>The dropdown list shown above contains the name of every quest available in the database. If you know the name of the quest you want to see more information about, this method will work for you. You can also use this method to select a random quest or one with an interesting name. Once you have one selected, click the 'Search' button to the right of the dropdown. You will be taken to the quest's information page.</p><br />
+<hr>
+<h3>Step 3b: By zone method:</h3>
+<img src="guideimages/guide_browsequest_4.png" border="4"/>
+<p>If you, for example, have many client orders in a specific zone, you can search for quests by zone. Click the dropdown and select one of the zones. Once you select a zone, click the 'Search' button right next to the dropdown list. Don't see the zone you're looking for? Some quests have not been entered yet. Try again later or <a href="contact.php">contact me</a> to let me know that you want a specific zone added.</p>
+<img src="guideimages/guide_browsequest_5.png" border="4"/>
+<p>Once you click the 'Search' button, you will see a link to search again and a list of quest names. These are the quests in the zone you selected. Click one to go to the quest's page and see more information about it.</p><br />
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
