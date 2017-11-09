ad78e6f208fb485d8b5262bc53b1d746b51b3cf8
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:03:49 -0500

    guide_browseco

---
+++ guide_browseco.php
@@ -0,0 +1,47 @@
+<?php
+
+// This page is a guide for browsing client orders.
+
+$page_title = 'CO Browsing Guide';
+include ('includes/header.php');
+?>
+
+<h1>Browsing Client Orders</h1>
+<h4></h4>
+
+<fieldset>
+<h3>Step 1: Click on the 'Search' button at the top or head to <a href="https://phantasy.online/search.php">phantasy.online/search.php</a>.</h3>
+<img src="guideimages/guide_browse_1.png" border="4"/>
+<p></p><br />
+<hr>
+<h3>Step 2: Search using one of three available methods:</h3>
+<img src="guideimages/guide_browseco_2.png" border="4"/>
+<p>You can search for a client order by name (3a), NPC (3b), or zone (3c). To see a specific method,
+ head to one of those three steps.</p><br />
+<hr><hr>
+<h3>Step 3a: By name method:</h3>
+<img src="guideimages/guide_browseco_3.png" border="4"/>
+<p>The dropdown pictured here contains the name of every repeatable client order in the database. Select one, then click the 'Search' button next to the dropdown to go to that client order's page.</p>
+<br />
+<hr>
+<h3>Step 3b: By NPC method:</h3>
+<img src="guideimages/guide_browseco_4.png" border="4"/>
+<p>If you either know which NPC offers your client order or you want to find out which client orders are repeatable that can be accepted from a specific NPC, you can select an NPC's name from the drop down list pictured above. Once you have on selected, click the 'Search' button directly to the right of the drop down.</p>
+<img src="guideimages/guide_browseco_5.png" border="4"/>
+<p>After you have pressed the button, a list will appear of that NPC's available repeatable client orders, in addition to the zone and rewards of each. To view more information about a client order,
+ click its name.</p><br />
+<hr>
+<h3>Step 3c: By zone method:</h3>
+<img src="guideimages/guide_browseco_6.png" border="4"/>
+<p>Towards the bottom of a page is a dropdown containing the zones in PSO2 that offer regular repeatable client orders. Select a zone from this dropdown list, then click the 'Search' button right next to it.</p>
+<p>A list will be provided of all the client orders available in that zone, including the NPC name and rewards. To view more about a particular order, click its name.</p><br />
+<img src="guideimages/guide_browseco_7.png" border="4"/><hr><hr>
+<h3>Step 4:</h3>
+<p>On the client order's page, you can view several pieces of information about that client order, including the zone, rewards, and which NPC offers that client order. In addition, the box at the bottom of the page has a list of quests in the same zone as that client order. Clicking the NPC or a quest's name will take you to the corresponding page for the item you click.</p>
+
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
