4d42044b9e237eb952c2605b099f5c07f456730c
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:28:56 -0500

    termsofuse

---
+++ termsofuse.php
@@ -0,0 +1,22 @@
+<?php
+
+// This page is simple -- it just shows the terms of use for the site.
+
+$page_title = 'Terms of Use';
+include ('includes/header.php');
+
+?>
+
+<h1>Terms of Use</h1>
+<fieldset><div id="terms" align="left">
+<h3>Thank you for expressing interest in this site. If you wish to register and help me gather data, I appreciate it and only ask two things of you.</h3>
+<h4>1. Don't sign up using a vulgar name. Usernames are visible to anyone visiting the site, and I won't have slurs or insensitive language on my website. This website is for a school project, and I won't have some jerk getting me bad marks because they signed up with a bad name.</h4>
+<h4>2. Don't fudge numbers. I know measuring the time it takes to complete a quest can be annoying, but if you enter a quest clear taking 10 seconds, I'm going to delete it. Seabed exploration doesn't take less time than it does to accept the quest and head to the ship. I very much would like to use this website to help me in PSO2, and false data doesn't help anyone.</h4>
+<h3>If you continue to register, you are agreeing to these terms.</h3>
+</div></fieldset><br />
+
+<?php
+
+include ('includes/footer.php');
+
+?>
