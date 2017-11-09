41e9f0a75764287a521e4ee23b71de04cff8c9ae
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:03:55 -0500

    guide_browsenpc

---
+++ guide_browsenpc.php
@@ -0,0 +1,29 @@
+<?php
+
+// This is a brief guide for browsing NPCs.
+
+$page_title = 'NPC Browsing Guide';
+include ('includes/header.php');
+?>
+
+<h1>Browsing NPCs</h1>
+<h4></h4>
+
+<fieldset>
+<h3>Step 1: Click on the 'Search' button at the top or head to <a href="https://phantasy.online/search.php">phantasy.online/search.php</a>.</h3>
+<img src="guideimages/guide_browse_1.png" border="4"/>
+<p></p><br />
+<hr>
+<h3>Step 2: Select an NPC from the drop down list.</h3>
+<img src="guideimages/guide_browseco_4.png" border="4"/>
+<p></p><br />
+<hr>
+<h3>Step 3: Click the 'Search' button to the right of the drop down.</h3>
+<img src="guideimages/guide_browsenpc_3.png" border="4"/>
+<p></p><br />
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
