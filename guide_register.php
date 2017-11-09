1db3e888dd2cff63a5029e08dd61bafef28e8579
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:04:22 -0500

    guide_register

---
+++ guide_register.php
@@ -0,0 +1,31 @@
+<?php
+
+// A guide for registering to the site.
+
+$page_title = 'Register Guide';
+include ('includes/header.php');
+?>
+
+<h1>How to Register</h1>
+<h4>Want an account so you can submit data? Here's how.</h4>
+
+<fieldset>
+<h3>Step 1: Either click on the 'register' link or head to <a href="http://phantasy.online/register.php">phantasy.online/register.php.</a></h3>
+<img src="guideimages/guide_register_1.png" border="4"/>
+<p>Here you will see a form with various fields to enter. Either register on your own, or continue through the guide if you're not sure what goes in each field.</p><br />
+<hr>
+<h3>Step 2: Enter information into the fields.</h3>
+<img src="guideimages/guide_register_2.png" border="4"/>
+<p>Your <b>username</b> is a unique 'name' that identifies you. When you submit data, it will be tied to this username. Do well, and you can show off your times! Next is your <b>email</b>, which is used for login and for the admin to contact you. Registration also requires a <b>password</b>, which you must type in twice to confirm. This is also used for login, and will help keep your account secure. The next item is a <b>security phrase</b>. This is used for authentication in the event that you lose access to your account</p>
+<p>The last item on the list is a dropdown and a question. To register, you must correctly answer the question. You can find the answer by thoroughly reading the <a href="http://phantasy.online/termsofuse.php">terms of use</a> page. If you register, that means I will assume that you have read these terms. Inappropriate names or suspicious quest run times are subject to alteration or removal.</p><br />
+<hr>
+<h3>Step 3: Click Register.</h3>
+<img src="guideimages/guide_register_3.png" border="4"/>
+<p>At the bottom of the registration form is the submit button, labelled 'Register'. Use this to submit your fields and create your account. Red text will pop up to notify you in the event that one of your fields is entered incorrectly.</p>
+<p>Once you're done, it's time to log in. You can <a href="http://phantasy.online/login.php">log in here</a> or <a href="guide_login.php">view a guide here</a>.</p>
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
