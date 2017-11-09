05087d41555e3b261e4f10667a38016bffb64cef
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:21:52 -0500

    otherresources

---
+++ otherresources.php
@@ -0,0 +1,36 @@
+<?php
+
+// This page lists extra resources for PSO2 as well as what resources I used during development.
+
+$page_title = 'Other Resources';
+include ('includes/header.php');
+
+?>
+
+<h1>Other Resources</h1>
+
+<div id="ResourcesPSO2" align="center"><fieldset>
+
+<h1><b>PSO2</b></h1><h3>More resources for Phantasy Star Online 2.</h3>
+<h3><a href="https://pso2.arks-visiphone.com/"> PSO2 Arks Visiphone EN </a></h3>
+<h3><a href="http://phantasystar.wikia.com/wiki/Phantasy_Star_Online_2"> PSO Wiki EN </a></h3>
+<h3><a href="http://srpgp.supersanctuary.net/cirnopedia-test/index.php"> Cirnopedia (Backup) </a></h3>
+<h3><a href="http://pso2wiki.net/"> PSO2 JP Wiki </a></h3>
+<h3><a href="http://pso2.swiki.jp/"> PSO2 JP SWiki </a></h3>
+</fieldset></div>
+<br />
+<div id="ResourcesWeb" align="center"><fieldset>
+
+<h1><b>Web Development</b></h1><h3>These resources were used in the development of this website.</h3>
+<h3><a href="https://stackoverflow.com/"> Stack Overflow </a></h3>
+<h3><a href="http://www.larryullman.com/"> Larry Ullman </a></h3>
+<h3><a href="https://www.w3schools.com/"> W3Schools </a></h3>
+<h3><a href="http://www.echoecho.com/"> Echo Echo </a></h3>
+<h3><a href="https://www.appdynamics.com/"> AppDynamics </a></h3>
+</fieldset></div><br />
+
+<?php
+
+include ('includes/footer.php');
+
+?>
