ed51a0b4f5109d52720ca71e36cc7553d4cd4138
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:04:34 -0500

    guide_viewdata

---
+++ guide_viewdata.php
@@ -0,0 +1,24 @@
+<?php
+
+// A very simple guide for viewing quest data.
+
+$page_title = 'Data View Guide';
+include ('includes/header.php');
+?>
+
+<h1>Viewing Quest Data</h1>
+<h4>Want to view data collected by PSO2 players about quests? Learn about your options here.</h4>
+
+<fieldset>
+<h3>Either click on the 'View Data' link or head to <a href="http://phantasy.online/viewruns.php">phantasy.online/viewruns.php</a>.</h3>
+<img src="guideimages/guide_viewdata_1.png" border="4"/>
+<p>On this page are two options. You can either <b>view all runs separately</b> or <b>view quest runs sorted by quest</b>. Select which you would like to view depending on what information you'd like to see. In separate runs, you can see individual run times, experience and meseta games, and the usernames of those running. In contrast, the grouped page displays the average of data for each quest.</p>
+<img src="guideimages/guide_viewdata_2.png" border="4"/>
+<p>Both views of quest data have buttons to sort the data. In addition, clicking the quest name will take you to a page with various pieces of information about that quest, such as the zone it's in or the client orders that can be completed alongside that quest. More guides are available for <a href="guide_browsequest.php">quest</a>, <a href="guide_browseco.php">client order</a>, and <a href="guide_browsenpc">NPC</a> pages.</p><br />
+
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
