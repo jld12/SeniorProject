9eac1122f332a8727cc33fcc1c9afdcd0303c0a9
jld12 <jenndaniel94@gmail.com>
2017-11-08 23:04:09 -0500

    guide_changepassword

---
+++ guide_changepassword.php
@@ -0,0 +1,39 @@
+<?php
+
+// A guide for changing one's password.
+
+$page_title = 'Password Change Guide';
+include ('includes/header.php');
+?>
+
+<h1>Changing Your Password</h1>
+<h4></h4>
+
+<fieldset>
+<h3>Step 1: Click on the 'Account' button at the top, then click 'Change Password' towards the top of the page, or head to <a href="https://phantasy.online/changepassword.php">phantasy.online/changepassword.php</a>.</h3>
+<img src="guideimages/guide_changepassword_1.png" border="4"/>
+<img src="guideimages/guide_changepassword_2.png" border="4"/>
+<p>On this page, you will see a simple form. Complete it, or continue through this guide to learn how.</p><br />
+<hr>
+<h3>Step 2: Enter your current password.</h3>
+<img src="guideimages/guide_changepassword_3.png" border="4"/>
+<p>To prevent people from messing with your account without your permission, you are required to enter your current password into the change password form. This ensures that whomever is changing your password already knows your current one. <a href="contact.php">If your password has changed without your knowledge, contact me</a>. If you know your security phrase, I can help you change your password.</p><br />
+<hr>
+<h3>Step 3: Enter a new password.</h3>
+<img src="guideimages/guide_changepassword_4.png" border="4"/>
+<p>In the box next to 'New Password,' enter what you would like your new password to be. Keep in mind that passwords are limited in size, so if you see that the box isn't getting any more dots, you've reached the length limit.</p><br />
+<hr>
+<h3>Step 4: Enter that same password into the box next to 'Verify Password.'</h3>
+<img src="guideimages/guide_changepassword_5.png" border="4"/>
+<p>To make sure you entered the password that you wanted to free of typos or mistakes, reenter your desired new password.</p><br />
+<hr>
+<h3>Step 5: Click the 'Change Password' button.</h3>
+<img src="guideimages/guide_changepassword_6.png" border="4"/>
+<p>If you have entered the fields correctly, green text will pop up at the top of the page to let you know that your password was updated. If something went wrong, it will also let you know, but with red text. Please note that entering the same password for your current and new password will result in a 'wrong credentials' message. After your password is changed, when you login next, you will be expected to use that new password.</p><br />
+
+</fieldset><br />
+
+
+<?php
+include ('includes/footer.php');
+?>
